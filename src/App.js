6d629a0272cd6c2ef69d928d9fa56bfcd0f6be3f
Sunil Shrestha <ilamalisunilshrestha00@gmail.com>
2018-03-25 11:27:22 +0545

    added new react project

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+import styles from './Assets/styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        these is header class 
+        </header>
+       
+        <p>these is body part of the reactjs</p>
+         <p>these is body part of the reactjs</p>
+          <p>these is body part of the reactjs</p>
+           <p>these is body part of the reactjs</p>
+            <p>these is body part of the reactjs</p>
+
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <input type="button" value="Add ME"/>
+           
+           <img src="jacket1.jpg"  alt="jacket"/> 
+          
+        <img src="../Assets/images/onlineshop.jpg"  alt="Profile image"/>
+      <footer className="App-footer">
+    <h1 className="App-Fname">these is footer portion</h1>
+    </footer>
+      </div>
+    
+    );
+  }
+}
+
+export default App;
---
+++ src/Assets/js/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        THES IS HEADER 
+        </header>
+       
+        <p>these is body part of the reactjs</p>
+         <p>these is body part of the reactjs</p>
+          <p>these is body part of the reactjs</p>
+           <p>these is body part of the reactjs</p>
+            <p>these is body part of the reactjs</p>
+
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <p>these is body part of the reactjs</p>
+             <input type="button" value="Add ME"/>
+           
+           <img src={"jacket1.jpg"}  alt="jacket"/> 
+          
+
+      <footer className="App-footer">
+    <h1 className="App-Fname">these is footer portion</h1>
+    </footer>
+      </div>
+    
+    );
+  }
+}
+
+export default App;
